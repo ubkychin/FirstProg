16d91d9517506a7ab8c41fb7e8509d1ee619311c
Anh Nguyen <anguyen@swin.edu.au>
2020-02-25 16:27:42 +1100

    reads and prints first and second name

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstProg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your firstname?");
+            string firstname = Console.ReadLine();
+
+            Console.WriteLine("What's your surname?");
+            string surname = Console.ReadLine();
+
+            string fullname = firstname + " " + surname;
+
+            Console.Write(fullname);
+        }
+    }
+}
